7a64328beeeca501f698cd1bdde3251905f620b5
swapnilugalmugale2002@gmail.com <swapnilugalmugale2002@gmail.com>
2024-08-13 14:49:16 +0530

    added the feign client and eureka discovery client

--- weshopify-brands-service/src/main/java/com/weshopify/platform/WeshopifyBrandsServiceApplication.java
+++ weshopify-brands-service/src/main/java/com/weshopify/platform/WeshopifyBrandsServiceApplication.java
@@ -2,6 +2,7 @@ package com.weshopify.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableFeignClients
 public class WeshopifyBrandsServiceApplication {
 
 	
--- weshopify-brands-service/src/main/java/com/weshopify/platform/service/BrandsServiceImpl.java
+++ weshopify-brands-service/src/main/java/com/weshopify/platform/service/BrandsServiceImpl.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -17,22 +20,29 @@ import com.weshopify.platform.bean.BrandsBean;
 import com.weshopify.platform.bean.CategoryBean;
 import com.weshopify.platform.model.Brands;
 import com.weshopify.platform.outbound.CategoriesApiClient;
+import com.weshopify.platform.outbound.CategoriesApiFeignClient;
 import com.weshopify.platform.repository.BrandsRepo;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class BrandsServiceImpl implements BrandsService {
-	
+
 	@Autowired
 	private BrandsRepo brandsRepo;
-	
+
+//	@Autowired
+//	private CategoriesApiClient catApiClient;
+
+	@Value("${services.categories.access-token}")
+	private String accessToken;
+
 	@Autowired
-	private CategoriesApiClient catApiClient;
-	
+	private CategoriesApiFeignClient apiFeignClient;
+
 	@Autowired
 	private ObjectMapper mapper;
-	
+
 	@Override
 	public BrandsBean createBrand(BrandsBean brandsBean) {
 		return convertEntityToBean(brandsRepo.save(convertBeanToEntity(brandsBean)));
@@ -46,13 +56,13 @@ public class BrandsServiceImpl implements BrandsService {
 	@Override
 	public List<BrandsBean> findAllBrands() {
 		List<Brands> brandsList = brandsRepo.findAll();
-		if(!CollectionUtils.isEmpty(brandsList)) {
+		if (!CollectionUtils.isEmpty(brandsList)) {
 			List<BrandsBean> brandBeanList = new ArrayList<>();
-			brandsList.stream().forEach(brand->{
+			brandsList.stream().forEach(brand -> {
 				brandBeanList.add(convertEntityToBean(brand));
 			});
 			return brandBeanList;
-		}else {
+		} else {
 			log.info("No Brands Available in DB");
 			throw new RuntimeException("No Brands Available in DB");
 		}
@@ -66,12 +76,48 @@ public class BrandsServiceImpl implements BrandsService {
 
 	@Override
 	public List<BrandsBean> deleteBrand(int brandId) {
-	    brandsRepo.deleteById(brandId);
+		brandsRepo.deleteById(brandId);
 		return findAllBrands();
 	}
-	
+
 	/**
-	 * converting the bean to entity model
+	 * converting the bean to entity model and making a RestTemplate call to
+	 * category service
+	 * 
+	 * @param catBean
+	 * @return
+	 */
+	/*
+	 * 
+	 * private Brands convertBeanToEntity(BrandsBean brandsBean) {
+	 * 
+	 * Brands brandsEntity = new Brands();
+	 * brandsEntity.setName(brandsBean.getName());
+	 * brandsEntity.setLogoPath(brandsBean.getLogoPath());
+	 * 
+	 *//**
+		 * Invoke the category service and fetch the categories from categories service
+		 *//*
+			 * if(!CollectionUtils.isEmpty(brandsBean.getCategories())) { List<CategoryBean>
+			 * orignalCats = new ArrayList<CategoryBean>();
+			 * brandsBean.getCategories().parallelStream().forEach(catBean->{ String catResp
+			 * = catApiClient.findCategoryById(null, catBean.getId()); try {
+			 * orignalCats.add(mapper.readValue(catResp,CategoryBean.class)); } catch
+			 * (JsonMappingException e) { // TODO Auto-generated catch block
+			 * e.printStackTrace(); } catch (JsonProcessingException e) { // TODO
+			 * Auto-generated catch block e.printStackTrace(); } });
+			 * brandsEntity.setCategories(orignalCats); }
+			 * if(StringUtils.hasText(brandsBean.getId())) {
+			 * brandsEntity.setId(brandsBean.getId()); }
+			 * 
+			 * return brandsEntity;
+			 * 
+			 * }
+			 */
+
+	/**
+	 * converting the bean to entity model and making feign client call to category
+	 * service
 	 * 
 	 * @param catBean
 	 * @return
@@ -81,36 +127,44 @@ public class BrandsServiceImpl implements BrandsService {
 
 		Brands brandsEntity = new Brands();
 		brandsEntity.setName(brandsBean.getName());
-		brandsEntity.setLogoPath(brandsBean.getLogoPath()); 
-		
+		brandsEntity.setLogoPath(brandsBean.getLogoPath());
+
 		/**
-		 * Invoke the category service and fetch the categories
-		 * from categories service
+		 * Invoke the category service and fetch the categories from categories service
 		 */
-		if(!CollectionUtils.isEmpty(brandsBean.getCategories())) {
+		if (!CollectionUtils.isEmpty(brandsBean.getCategories())) {
 			List<CategoryBean> orignalCats = new ArrayList<CategoryBean>();
-			brandsBean.getCategories().parallelStream().forEach(catBean->{
-				String catResp = catApiClient.findCategoryById(null, catBean.getId());
-			     try {
-			    	 orignalCats.add(mapper.readValue(catResp,CategoryBean.class));
-				} catch (JsonMappingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (JsonProcessingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			Map<String, String> headerMap = new HashMap<>();
+			headerMap.put("Authorization", "Bearer " + accessToken);
+			
+			brandsBean.getCategories().parallelStream().forEach(catBean -> {
+
+				long startTime = System.currentTimeMillis();
+				ResponseEntity<String> catRespEntity = apiFeignClient.findCategoryById(catBean.getId(), headerMap);
+				long endTime = System.currentTimeMillis();
+				log.info("Total time taken by the category service feign client {}", (endTime-startTime));
+				if (catRespEntity != null && HttpStatus.OK.value() == catRespEntity.getStatusCode().value()) {
+					try {
+						orignalCats.add(mapper.readValue(catRespEntity.getBody(), CategoryBean.class));
+					} catch (JsonMappingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (JsonProcessingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
+
 			});
 			brandsEntity.setCategories(orignalCats);
 		}
-		if(StringUtils.hasText(brandsBean.getId())) {
+		if (StringUtils.hasText(brandsBean.getId())) {
 			brandsEntity.setId(brandsBean.getId());
 		}
-		
+
 		return brandsEntity;
 
 	}
-	
 
 	/**
 	 * converting the entity model to bean
@@ -126,11 +180,9 @@ public class BrandsServiceImpl implements BrandsService {
 		brandsBean.setLogoPath(brandsEntity.getLogoPath());
 		brandsBean.setCategories(brandsEntity.getCategories());
 		brandsBean.setId(brandsEntity.getId());
-		
-		return brandsBean;
-				
-	}
-	
 
-	
+		return brandsBean;
+
+	}
+
 }
